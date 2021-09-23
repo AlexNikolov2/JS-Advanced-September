0802582e6c0f23502c95afe214b3bdf36e45bdc7
Aleks Nikolov <tcr.official.world@gmail.com>
2021-09-23 18:39:21 +0300

    commit

--- Objects/10.Heroes.js
+++ Objects/10.Heroes.js
@@ -1,5 +1,41 @@
 function solve(){
     return{
-        fighter
+        fighter(name){
+            return{
+                'name': name,
+                'health': 100,
+                'stamina': 100,
+                fight(){
+                    console.log(`${this.name} slashes at the foe!`);
+                    this.stamina -= 1;
+                }
+            }
+            
+
+        },
+        mage(name){
+            return{
+                'name': name,
+                'health': 100,
+                'mana': 100,
+                cast(spell){
+                    console.log(`${this.name} cast ${spell}`);
+                    this.mana -= 1;
+                }
+            }
+        }
     }
+    let a = [];
+    a.unshift
 }
+let create = solve();
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball")
+scorcher.cast("thunder")
+scorcher.cast("light")
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight()
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
