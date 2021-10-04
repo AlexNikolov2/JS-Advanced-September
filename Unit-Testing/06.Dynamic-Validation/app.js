1bc3334e8f82041bed940f9ee08bed78614fc4ab
Aleks Nikolov <tcr.official.world@gmail.com>
2021-10-04 23:32:56 +0300

    commit

--- Unit-Testing/06.Dynamic-Validation/app.js
+++ Unit-Testing/06.Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    // TODO
+    let regex = /^[a-z]+@[a-z]+\.[a-z]+/;
+    let inputElement = document.getElementById('email');
+    inputElement.addEventListener('change', () =>{
+        let result = inputElement.value.match(regex);
+        if(result === null){
+            inputElement.style.borderColor = 'red';
+            inputElement.className = 'error';
+        }
+        else{
+            inputElement.value = '';
+            inputElement.removeAttribute('class');
+        }
+    })
+    
 }
