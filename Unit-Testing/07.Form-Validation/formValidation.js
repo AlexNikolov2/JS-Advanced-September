03043aca9275a61a9191f3fcfbe782e865412da0
Aleks Nikolov <tcr.official.world@gmail.com>
2021-10-07 23:36:27 +0300

    commit

--- Unit-Testing/07.Form-Validation/formValidation.js
+++ Unit-Testing/07.Form-Validation/formValidation.js
@@ -1,3 +1,6 @@
 function validate() {
-    // TODO
+    // validation for username 3-20 letters and nums
+    // password Validation 5-15 symbols(words, nums, _)
+    // email validation, use ex 6
+    //
 }
