a5b936ac05f0706a45fa3c6bacd109871708a4e6
Aleks Nikolov <tcr.official.world@gmail.com>
2021-10-08 18:56:15 +0300

    commit, unfinished task

--- Unit-Testing/07.Form-Validation/formValidation.js
+++ Unit-Testing/07.Form-Validation/formValidation.js
@@ -1,6 +1,76 @@
 function validate() {
     // validation for username 3-20 letters and nums
-    // password Validation 5-15 symbols(words, nums, _)
+    // password Validation 5-15 symbols(words, nums, _), must match with repeat pass
     // email validation, use ex 6
-    //
+    //is company - company number between 1000 and 9999
+
+    let username = document.getElementById('username');
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let confirmPassword = document.getElementById('confirm-password');
+    let companyInfo = document.getElementById('companyInfo');
+    let company = document.getElementById('company');
+    let companyNumber = document.getElementById('company-number');
+    let validElement = document.getElementById('valid')
+
+    let userregex = /[a-zA-Z0-9]{3,20}/gm;
+    let passregex = /\w{5,15}/;
+    let emailregex = /^[a-z]+@[a-z]+\.[a-z]+/;
+
+    let isUserValid = Boolean;
+    let isPassValid = Boolean;
+    let isEmailValid = Boolean;
+    let isRepValid = Boolean;
+
+    username.addEventListener('change', (event) =>{
+        event.preventDefault();
+        let result = username.value.match(userregex);
+        if(result === null){
+            username.style.borderColor = 'red';
+            username.className = 'error';
+            validElement.style.display = 'none';
+        }
+        else{
+            username.value = '';
+            username.style.border = 'none';
+            isUserValid = true;
+        }
+    })
+    password.addEventListener('change', (event) =>{
+        event.preventDefault();
+        let result = password.value.match(passregex);
+        if(result === null){
+            password.style.borderColor = 'red';
+            password.className = 'error';
+            validElement.style.display = 'none';
+        }
+        else{
+            password.value = '';
+            password.style.border = 'none';
+            isPassValid = true;
+        }
+    })
+    if(password.value == confirmPassword.value){
+        confirmPassword.value = '';
+        confirmPassword.style.border = 'none';
+        isRepValid = true;
+    }
+    else{
+        confirmPassword.style.borderColor = 'red';
+        confirmPassword.className = 'error';
+        validElement.style.display = 'none';
+    }
+    email.addEventListener('change', () =>{
+        let result = email.value.match(emailregex);
+        if(result === null){
+            email.style.borderColor = 'red';
+            email.className = 'error';
+            validElement.style.display = 'none';
+        }
+        else{
+            email.value = '';
+            email.style.border = 'none';
+            isEmailValid = true;
+        }
+    })
 }
