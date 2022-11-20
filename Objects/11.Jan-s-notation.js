8ef62338a3aac0394a910d76b48c0942d93ede24
Aleks Nikolov <tcr.official.world@gmail.com>
2022-11-21 00:07:46 +0200

    11

--- Objects/11.Jan-s-notation.js
+++ Objects/11.Jan-s-notation.js
@@ -1,59 +1,34 @@
-function solve(input){
+function solve(input) {
+  let operators = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+  };
 
-    let operators = {
-        '+' : (a,b) => a+b,
-        '-' : (a,b) => a-b,
-        '*' : (a,b) => a*b,
-        '/' : (a,b) => a/b,
+  let array = [];
+  let error = false;
+  for (const element of input) {
+    if (Number.isInteger(element)) {
+      array.push(element);
+    } else {
+      if (array.length < 2) {
+        console.log("Error: not enough operands!");
+        error = true;
+      }
+      let first = array.pop();
+      let second = array.pop();
+      array.push(operators[element](second, first));
     }
-
-    let array = [];
-    let error = false;
-    for (const element of input) {
-        if(Number.isInteger(element)){
-            array.push(element);
-        }
-        else{
-            if(array.length < 2){
-                console.log('Error: not enough operands!');
-                error = true;
-
-            }
-            let first = array.pop();
-            let second = array.pop();
-            array.push(operators[element](second,first));
-
-        }
-    }
-    if(array.length === 1 && error === false){
-       console.log(array[0]);
-    }else if (error === false){
-        console.log('Error: too many operands!');
-    }
-
-
-
+  }
+  if (array.length === 1 && error === false) {
+    console.log(array[0]);
+  } else if (error === false) {
+    console.log("Error: too many operands!");
+  }
 }
-solve([15,
-    '/']
+solve([15, "/"]);
+solve([3, 4, "+"]);
+solve([5, 3, 4, "*", "-"]);
 
-
-               
-);
-solve(
-    [3,
-        4,
-        '+'])
-solve(
-
-    [5,
-        3,
-        4,
-        '*',
-        '-'])
-
-        solve(
-            [7,
-                33,
-                8,
-                '-'])
+solve([7, 33, 8, "-"]);
