2be2a75c4200fda32bacb09698d81ff1dcafb9bc
Aleks Nikolov <tcr.official.world@gmail.com>
2022-11-23 18:17:39 +0200

    02

---
+++ Arrays/02.js
@@ -0,0 +1,17 @@
+function increment(arr, step){
+    let newArray = [];
+    for (let i = 0; i < arr.length; i+=step) {
+        const element = arr[i];
+        newArray.push(element)
+    }
+    console.log(newArray);
+}
+increment(
+    ['5', 
+    '20', 
+    '31', 
+    '4', 
+    '20'], 
+    2
+    
+)
